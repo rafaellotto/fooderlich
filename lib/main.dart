5ddca2b493c438f7649cc5d228d621e98b01101b
Rafael Lotto Machado <ralomach@gmail.com>
2021-10-10 17:51:10 -0300

    Changes from item 7.12

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/navigation/app_router.dart';
 import 'package:provider/provider.dart';
 
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
-import 'screens/splash_screen.dart';
-// TODO: Import app_router
 
 void main() {
   runApp(
@@ -23,9 +22,18 @@ class _FooderlichState extends State<Fooderlich> {
   final _groceryManager = GroceryManager();
   final _profileManager = ProfileManager();
   final _appStateManager = AppStateManager();
-  // TODO: Define AppRouter
+  late AppRouter _appRouter;
 
-  // TODO: Initialize app router
+  @override
+  void initState() {
+    super.initState();
+
+    _appRouter = AppRouter(
+      appStateManager: _appStateManager,
+      groceryManager: _groceryManager,
+      profileManager: _profileManager,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,8 +55,8 @@ class _FooderlichState extends State<Fooderlich> {
           return MaterialApp(
             theme: theme,
             title: 'Fooderlich',
-            // TODO: Replace with Router widget
-            home: const SplashScreen(),
+            home: Router(routerDelegate: _appRouter),
+            // TODO: Add backButtonDispatcher
           );
         },
       ),
