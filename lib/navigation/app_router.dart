d5fbb965b14889423464dcc94461ab78e2aa06c7
Rafael Lotto Machado <ralomach@gmail.com>
2021-10-10 19:36:14 -0300

    Changes from item 7.19

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -47,8 +47,28 @@ class AppRouter extends RouterDelegate
         if (appStateManager.isOnboardingComplete)
           Home.page(appStateManager.getSelectedTab),
 
-        // TODO: Create new item
-        // TODO: Select GroceryItemScreen
+        if (groceryManager.isCreatingNewItem)
+          GroceryItemScreen.page(
+            onCreate: (item) {
+              groceryManager.addItem(item);
+            },
+            onUpdate: (item, index) {
+              // No update
+            },
+          ),
+
+        if (groceryManager.selectedIndex != -1)
+          GroceryItemScreen.page(
+            item: groceryManager.selectedGroceryItem,
+            index: groceryManager.selectedIndex,
+            onCreate: (_) {
+              // No create
+            },
+            onUpdate: (item, index) {
+              groceryManager.updateItem(item, index);
+            },
+          ),
+
         // TODO: Add Profile Screen
         // TODO: Add WebView Screen
       ],
@@ -64,7 +84,10 @@ class AppRouter extends RouterDelegate
       appStateManager.logout();
     }
 
-    // TODO: Handle state when user closes grocery item screen
+    if (route.settings.name == FooderlichPages.groceryItemDetails) {
+      groceryManager.groceryItemTapped(-1);
+    }
+
     // TODO: Handle state when user closes profile screen
     // TODO: Handle state when user closes WebView screen
 
